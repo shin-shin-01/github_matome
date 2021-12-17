e9727181dc2d84b965acf3cf9316397610704b4b
Kaze Shindo <kaze.bknb@outlook.jp>
2021-12-17 13:57:44 +0900

    refactor textStyle

--- lib/ui/home/home_elements.dart
+++ lib/ui/home/home_elements.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:github_matome/ui/theme/app_theme.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // GitHubのアイコン
-class GitHubIcon extends StatelessWidget {
+class GitHubIcon extends HookConsumerWidget {
   const GitHubIcon({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(90),
       child: Image.network(
@@ -19,41 +21,42 @@ class GitHubIcon extends StatelessWidget {
 }
 
 // トータルコミット数
-class TotalCommitCount extends StatelessWidget {
+class TotalCommitCount extends HookConsumerWidget {
   const TotalCommitCount({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(appThemeProvider);
     return Column(
       children: [
         const Text("今年のコミット数は"),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             Text(
               "4000 ",
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h70.bold(),
             ),
             Text(
               "commit",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h50.bold(),
             ),
           ],
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             Text(
               "昨年 ",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h40,
             ),
             Text(
               "100 ",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h50.bold(),
             ),
             Text(
               "commit",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h40,
             ),
           ],
         ),
@@ -63,41 +66,42 @@ class TotalCommitCount extends StatelessWidget {
 }
 
 // 開発した日数
-class TotalCommitDays extends StatelessWidget {
+class TotalCommitDays extends HookConsumerWidget {
   const TotalCommitDays({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(appThemeProvider);
     return Column(
       children: [
         const Text("今年開発をした日数は"),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             Text(
               "300",
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h70.bold(),
             ),
             Text(
               "日",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h50.bold(),
             ),
           ],
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             Text(
               "昨年 ",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h40,
             ),
             Text(
               "100",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h50.bold(),
             ),
             Text(
               "日",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h40,
             ),
           ],
         ),
@@ -107,24 +111,25 @@ class TotalCommitDays extends StatelessWidget {
 }
 
 // 連続して開発した日数
-class ContinuousCommitDays extends StatelessWidget {
+class ContinuousCommitDays extends HookConsumerWidget {
   const ContinuousCommitDays({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(appThemeProvider);
     return Column(
       children: [
         const Text("今年最も連続で開発をした日数は"),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             Text(
               "30",
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h70.bold(),
             ),
             Text(
               "日",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h50.bold(),
             ),
           ],
         ),
@@ -134,30 +139,31 @@ class ContinuousCommitDays extends StatelessWidget {
 }
 
 // 最も開発した月
-class TotalCommitCountByMonth extends StatelessWidget {
+class TotalCommitCountByMonth extends HookConsumerWidget {
   const TotalCommitCountByMonth({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(appThemeProvider);
     final Size size = MediaQuery.of(context).size;
     final double betweenHeight = size.height * 0.1;
     return Column(
       children: [
         const Text("今年最も開発をした月は..."),
-        const Text(
+        Text(
           "January",
-          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          style: theme.textTheme.h70.bold(),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             Text(
               "2010",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h40,
             ),
             Text(
               "commits",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: theme.textTheme.h40,
             ),
           ],
         ),
@@ -168,20 +174,20 @@ class TotalCommitCountByMonth extends StatelessWidget {
           itemCount: 2,
           itemBuilder: (BuildContext context, _) => Column(
             children: [
-              const Text(
+              Text(
                 "Febrary",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                style: theme.textTheme.h50,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
+                children: [
                   Text(
                     "200",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    style: theme.textTheme.h40,
                   ),
                   Text(
                     "commits",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    style: theme.textTheme.h40,
                   ),
                 ],
               ),
