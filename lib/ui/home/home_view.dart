09d9547d41d4c912c7d69f354016eb2a1969a23a
Kaze Shindo <kaze.bknb@outlook.jp>
2021-12-17 17:13:04 +0900

    refactor appBar

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:github_matome/shared/app_bar.dart';
 import 'package:stacked/stacked.dart';
 import 'package:github_matome/ui/home/home_elements.dart';
 import 'package:github_matome/ui/home/home_viewmodel.dart';
@@ -18,27 +19,7 @@ class HomeView extends StatelessWidget {
       viewModelBuilder: () => HomeViewModel(),
       onModelReady: (model) => model.initialize(),
       builder: (context, model, child) => Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // GitHub Icon
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.asset(
-                  // TODO; fix here
-                  'assets/images/GitHub-Mark-Light-120px-plus.png',
-                  width: 30,
-                  height: 30,
-                  fit: BoxFit.fill,
-                ),
-              ),
-              const SizedBox(width: 20),
-              // Title
-              const Text('GitHub Summary'),
-            ],
-          ),
-        ),
+        appBar: customAppBar(),
         body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
