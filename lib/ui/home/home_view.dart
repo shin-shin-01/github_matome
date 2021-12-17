40cc839419c40b665ea9dfe996451cc39bb02f1b
Kaze Shindo <kaze.bknb@outlook.jp>
2021-12-17 10:53:58 +0900

    add: HomeView

---
+++ lib/ui/home/home_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'home_viewmodel.dart';
+
+class HomeView extends StatelessWidget {
+  static const routeName = '/home';
+
+  const HomeView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<HomeViewModel>.reactive(
+        viewModelBuilder: () => HomeViewModel(),
+        onModelReady: (model) => model.initialize(),
+        builder: (context, model, child) => const SizedBox());
+  }
+}
